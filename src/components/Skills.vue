2ac21b051aa513f3dc072ee45995546f644dc2ca
Aazib Majeed <133094615+Tenja001@users.noreply.github.com>
2025-09-07 15:09:03 +0500

    Skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -13,26 +13,32 @@
 </template>
 
 <script>
-import SkillCard from "./SkillCard.vue"  // ✅ Import your SkillCard component
+import SkillCard from "./SkillCard.vue"
 import htmlImg from "../assets/Skills/html.png"
 import cssImg from "../assets/Skills/css.png"
 import jsImg from "../assets/Skills/js.png"
 import vueImg from "../assets/Skills/vue.png"
-
+import cppImg from "../assets/Skills/Cpp.png"
+import pythonImg from "../assets/Skills/Python.png"
+import JavaImg from "../assets/Skills/Java.png"
+import E_Img from "../assets/Skills/Embedded_Systems.png"
 
 export default {
   name: "Skills",
-  components: { SkillCard },   // ✅ Register it here
+  components: { SkillCard },
   data() {
     return {
       skills: [
         { title: "HTML", image: htmlImg },
         { title: "CSS", image: cssImg },
         { title: "JavaScript", image: jsImg },
-        { title: "Vue.js", image: vueImg }
+        { title: "Vue.js", image: vueImg },
+        { title: "C++", image: cppImg },
+        { title: "Python", image: pythonImg },
+        { title: "Java", image:JavaImg},
+        { title: "Embedded Systems", image:E_Img}
       ]
     }
   }
 }
 </script>
-

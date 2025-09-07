88a36a6714eff37c88d0ad8246290898f680eec1
Aazib Majeed <133094615+Tenja001@users.noreply.github.com>
2025-09-07 15:08:31 +0500

    ProjectCard.vue

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,14 +1,31 @@
 <template>
-  <div class="card">
-    <h3>{{ title }}</h3>
-    <p>{{ description }}</p>
-    <a :href="link" target="_blank">View Project</a>
+  <div class="project-card">
+    <img :src="image" :alt="title" class="project-image" />
+    <div class="project-info">
+      <h3>{{ title }}</h3>
+      <p v-html="formatDescription(description)"></p>
+
+      <p class="tools">
+        <strong>Tools:</strong> {{ tools.join(", ") }}
+      </p>
+      <a :href="link" target="_blank" class="github-link">View on GitHub</a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "ProjectCard",
-  props: ["title", "description", "link"]
+  props: ["title", "description", "link", "tools", "image"],
+  methods: {
+    formatDescription(desc) {
+      return desc
+        .replace(/- /g, "• ")   // turn "- " into bullets
+        .replace(/\n/g, "<br>") // keep line breaks
+    }
+  }
 }
 </script>
+
+
+<style src="../style.css"></style>

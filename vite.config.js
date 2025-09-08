5558b2fed6169b6c98d800a12be074979fc23c47
Aazib Majeed <133094615+Aazib-Majeed@users.noreply.github.com>
2025-09-08 14:51:18 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Portfolio/'   // 👈 use your repo name here
+  base: './'   // 👈 use your repo name here
 })

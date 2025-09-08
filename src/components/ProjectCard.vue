530f108550ef8bdd68fe44521161ab5138fb671e
Aazib Majeed <133094615+Aazib-Majeed@users.noreply.github.com>
2025-09-08 16:03:24 +0500

    Update ProjectCard.vue

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="project-card">
-    <img :src="image" :alt="title" class="project-image" />
-    <div class="project-info">
-      <h3>{{ title }}</h3>
-      <p v-html="formatDescription(description)"></p>
+    <template v-if="mode === 'front'">
+      <img :src="image" :alt="title" class="project-image" />
+      <div class="project-info">
+        <h3>{{ title }}</h3>
+        <p class="tools"><strong>Tools:</strong> {{ tools.join(", ") }}</p>
+      </div>
+    </template>
 
-      <p class="tools">
-        <strong>Tools:</strong> {{ tools.join(", ") }}
-      </p>
-      <a :href="link" target="_blank" class="github-link">View on GitHub</a>
-    </div>
+    <template v-else>
+      <div class="project-info">
+        <h3>{{ title }}</h3>
+        <p v-html="formatDescription(description)"></p>
+        <p class="tools"><strong>Tools:</strong> {{ tools.join(", ") }}</p>
+        <a :href="link" target="_blank" class="github-link">View on GitHub</a>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: "ProjectCard",
-  props: ["title", "description", "link", "tools", "image"],
+  props: ["title", "description", "link", "tools", "image", "mode"],
   methods: {
     formatDescription(desc) {
       return desc
-        .replace(/- /g, "• ")   // turn "- " into bullets
-        .replace(/\n/g, "<br>") // keep line breaks
-    }
-  }
-}
+        .replace(/- /g, "• ") // convert dashes to bullets
+        .replace(/\n/g, "<br>");
+    },
+  },
+};
 </script>
-
 
 <style src="../style.css"></style>
